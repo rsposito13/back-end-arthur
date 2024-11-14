eb20d62f39d3b2bf387850239e17b9bb08bbd3a0
Arthur Porto <arthur.porto@ifnmg.edu.br>
2024-11-14 16:31:41 -0300

    rota enem

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   });
 });
 
+
+app.get("/enem", (req, res) => {        // Cria a rota da raiz do projeto
+  console.log("Rota GET/ solicitada");
+  res.json({
+    msg: "Você não precisa do ENEM para ser Ninguém!",      // Substitua pelo seu nome
+  });
+});
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
