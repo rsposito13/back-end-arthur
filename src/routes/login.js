e4c55b156b1cd197f9cab2f8ac0d2161e7156caf
rsposito13 <164542215+rsposito13@users.noreply.github.com>
2025-03-13 19:23:08 +0000

    arhur my amor

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { autenticarUsuario } from "../db/index.js";
+import verificarAutenticacao from "../middlewares/autenticacao.js"
 
 const router = Router();
 
@@ -18,5 +19,12 @@ router.post("/login", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-
+router.get("/auth", verificarAutenticacao, async (req, res) => {
+  console.log("Rota GET /auth solicitada");
+  try {
+    res.status(200).json({ user: `${req.userId}` });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 export default router;
